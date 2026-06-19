17a95abd8a0fc1a55a907c356ce833472be64607
Egor-Gorokh <gorokhegorwork@gmail.com>
2026-06-19 18:11:42 +0300

    add pr-check node22 and d

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.get('/ping', (req, res) => {
 });
  
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server ruvddvd nning on http://localhost:${PORT}`);
 });
  
 export default app;
