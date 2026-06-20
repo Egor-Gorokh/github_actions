6e75ff2e86eeee4fc59018ef863433b6fea1a849
Egor-Gorokh <gorokhegorwork@gmail.com>
2026-06-20 12:38:24 +0300

    fix 99

--- front/src/pages/todos-list.tsx
+++ front/src/pages/todos-list.tsx
@@ -49,7 +49,7 @@ export function TodosListPage() {
       >
         <div className="flex-1">
           <Input
-            placeholder="What needs to be done 8?"
+            placeholder="What needs to be done 9?"
             disabled={createTodo.isPending}
             data-testid="create-title"
             {...form.register('title')}
